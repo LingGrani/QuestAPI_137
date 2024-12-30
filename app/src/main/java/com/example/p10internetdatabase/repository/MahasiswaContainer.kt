ddbc254fb8b0ac9985599aa7aef67c680e6f5f31
LingGrani <luthfiangger2510@gmail.com>
2024-12-30 07:49:33 +0700

    Create MahasiswaContainer

---
+++ app/src/main/java/com/example/p10internetdatabase/repository/MahasiswaContainer.kt
@@ -0,0 +1,21 @@
+package com.example.p10internetdatabase.repository
+
+import com.example.p10internetdatabase.service.MahasiswaService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AppContainer{
+    val mahasiswaRepository: MahasiswaRepository
+}
+
+class MahasiswaContainer: AppContainer{
+    private val baseURL = "http://127.0.0.1/umyTI/"
+    private val json = Json { ignoreUnknownKeys = true }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseURL).build()
+    private val mahasiswaService: MahasiswaService by lazy { retrofit.create(MahasiswaService::class.java) }
+    override val mahasiswaRepository: MahasiswaRepository by lazy { NetworkMahasiswaRepository(mahasiswaService) }
+}
